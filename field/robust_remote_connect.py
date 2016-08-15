d61d88e59f070420e53ad2a0d573a7b225a17c41
Greg D. Hill <greghill@cs.stanford.edu>
2016-08-15 09:36:35 -0700

    bug fixes, validate ip address

--- field/robust_remote_connect.py
+++ field/robust_remote_connect.py
@@ -3,21 +3,24 @@ import argparse
 import time
 import os
 import sys
-from subprocess import Popen, call
+from subprocess import Popen, call, PIPE
 import re
+import ipaddress
 
 parser = argparse.ArgumentParser(description='todo')
 
-parser.add_argument('cloud_ip', type=str, help='IP address of cloud box')
-
 parser.add_argument('cloud_username', type=str, help='username to SSH into for cloud box')
+
+parser.add_argument('cloud_ip', type=ipaddress.ip_address, help='IP address of cloud box')
 
 args = parser.parse_args()
 
 os.environ["AUTOSSH_GATETIME"] = "0"
 os.environ["AUTOSSH_POLL"] = "60" # poll for a connection every 60 seconds
 
-autossh_process = popen(["autossh", args.cloud_username + "@" + args.cloud_ip, "-R", "0:localhost:22", "sleep"], stdout=PIPE, shell=True)
+autossh_cmd = ["autossh", args.cloud_username + "@" + str(args.cloud_ip), "-R", "0:localhost:22", "sleep"]
+print(" ".join(autossh_cmd))
+autossh_process = Popen(autossh_cmd, stdout=PIPE, shell=True)
 
 for line in autossh_process.stdout:
     match = re.search("Allocated port ([0-9]*)", line)
