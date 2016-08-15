dadf1486e1b5fe313afe5c89536b03f8ccf583fa
Greg D. Hill <greghill@cs.stanford.edu>
2016-08-15 10:08:52 -0700

    field script should be working now

--- field/robust_remote_connect.py
+++ field/robust_remote_connect.py
@@ -19,11 +19,11 @@ os.environ["AUTOSSH_GATETIME"] = "0"
 os.environ["AUTOSSH_POLL"] = "60" # poll for a connection every 60 seconds
 
 autossh_cmd = ["autossh", args.cloud_username + "@" + str(args.cloud_ip), "-R", "0:localhost:22", "sleep"]
-print(" ".join(autossh_cmd))
-autossh_process = Popen(autossh_cmd, stdout=PIPE, shell=True)
+autossh_process = Popen(autossh_cmd, stderr=PIPE, universal_newlines=True)
 
-for line in autossh_process.stdout:
+for line in autossh_process.stderr:
     match = re.search("Allocated port ([0-9]*)", line)
     if match:
         port = match.group(1)
-        call(["autossh", args.cloud_username + "@" + args.cloud_ip, "set_port", "--port", port]) # automatically retries on poor connection
+        set_port_cmd = ['autossh', args.cloud_username + '@' + str(args.cloud_ip), 'set_port --port '+ port+ '']
+        call(set_port_cmd) # autossh so automatically retries on poor connection
