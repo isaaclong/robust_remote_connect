2b287e604c565279390c46facc66a2f969af2e57
Greg D. Hill <greghill@cs.stanford.edu>
2016-08-15 14:29:49 -0700

    scripts and placeholders for authorized keys

---
+++ field/sync_authorized_keys.sh
@@ -0,0 +1,4 @@
+touch ~/.ssh/authorized_keys || exit
+sort -u ~/.ssh/authorized_keys ~/robust_remote_connect/cloud/authorized_keys > /tmp/.authorized_keys;
+mv /tmp/.authorized_keys ~/.ssh/authorized_keys;
+echo keys synced
