5a65e6db7ee2166da7837c149f6611b0f2590d3e
Greg D. Hill <greghill@cs.stanford.edu>
2016-08-16 02:41:02 -0700

    field connect loops if autossh returns

--- field/robust_remote_connect.py
+++ field/robust_remote_connect.py
@@ -18,12 +18,16 @@ args = parser.parse_args()
 os.environ["AUTOSSH_GATETIME"] = "0"
 os.environ["AUTOSSH_POLL"] = "60" # poll for a connection every 60 seconds
 
-autossh_cmd = ["autossh", args.cloud_username + "@" + str(args.cloud_ip), "-R", "0:localhost:22", "sleep"]
-autossh_process = Popen(autossh_cmd, stderr=PIPE, universal_newlines=True)
+while True: # in case autossh returns
+    autossh_cmd = ["autossh", args.cloud_username + "@" + str(args.cloud_ip), "-R", "0:localhost:22", "sleep"]
+    autossh_process = Popen(autossh_cmd, stderr=PIPE, universal_newlines=True)
 
-for line in autossh_process.stderr:
-    match = re.search("Allocated port ([0-9]*)", line)
-    if match:
-        port = match.group(1)
-        set_port_cmd = ['autossh', args.cloud_username + '@' + str(args.cloud_ip), 'set_port --port '+ port+ '']
-        call(set_port_cmd) # autossh so automatically retries on poor connection
+    for line in autossh_process.stderr:
+        match = re.search("Allocated port ([0-9]*)", line)
+        if match:
+            port = match.group(1)
+            set_port_cmd = ['autossh', args.cloud_username + '@' + str(args.cloud_ip), 'set_port --port '+ port+ '']
+            call(set_port_cmd) # autossh so automatically retries on poor connection
+
+
+    autossh_process.wait()
